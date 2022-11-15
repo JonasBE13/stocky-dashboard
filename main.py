87248a2fa82ce5790f016d10ce408314125cf0ea
Jonas Bengtstrom <jonasbengtstrom@gmail.com>
2022-11-15 11:12:56 +0100

    ready for deployment

--- main.py
+++ main.py
@@ -28,6 +28,8 @@ app = dash.Dash(
     meta_tags=[dict(name="viewports", content="width=device-width, initial-scale=1.0")],
 )
 
+server = app.server
+
 app.layout = Layout(symbol_dict).layout()
 
 
